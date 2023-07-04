01480bdf26e84fa1b1483eedefe5c30644e2da8f
kyrzawidg <138618825+kyrzawidg@users.noreply.github.com>
2023-07-05 01:49:21 +0700

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-cii6g959aq012eq5t88g-a",
+    USER: "widget_user",
+    PORT: 5432,
+    PASSWORD: "ZfCELYFofMVtmnCG5L68XCMEvs5urzP1",
+    DB: "widget",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
